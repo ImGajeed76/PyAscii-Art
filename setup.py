5a1a08c0c51d3d5e13db4c529cda03574d26f09d
ImGajeed76 <imgajeed.76@gmail.com>
2021-12-11 16:21:24 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="PyAscii-Art",
+    version="0.0.1",
+    description="Image to ASCII art images library.",
+    author="ImGajeed",
+    url=""
+)
---
+++ src/PyAscii-Art/hello_world.py
@@ -0,0 +1,11 @@
+from src.ascii_art import AsciiArt
+
+
+def helloWorld():
+    image_path = r"example_image.jpg"
+    ascii_art = AsciiArt(image_path, divider=3.7, spacerX=4, spacerY=1, ascii_chars_black_to_white=AsciiArt.simple_char_set)
+
+    ascii_art.generatePixelArray()
+    ascii_art.trimPixelColors()
+    ascii_art.generateAsciiImage()
+    ascii_art.writeToFile("image.txt")

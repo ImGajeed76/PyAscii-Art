9083d606d560b42d9497d4e0bb0889739a097b59
ImGajeed76 <imgajeed.76@gmail.com>
2021-12-11 17:29:25 +0100

    Initial Commit

--- src/pyAsciiArt/hello_world.py
+++ src/pyAsciiArt/hello_world.py
@@ -1,9 +1,10 @@
-from src.ascii_art import AsciiArt
+from src.pyAsciiArt.PyAsciiArt import AsciiArt
 
 
 def helloWorld():
     image_path = r"example_image.jpg"
-    ascii_art = AsciiArt(image_path, divider=3.7, spacerX=4, spacerY=1, ascii_chars_black_to_white=AsciiArt.simple_char_set)
+    ascii_art = AsciiArt(image_path, divider=3.7, spacerX=4, spacerY=1,
+                         ascii_chars_black_to_white=AsciiArt.simple_char_set)
 
     ascii_art.generatePixelArray()
     ascii_art.trimPixelColors()
